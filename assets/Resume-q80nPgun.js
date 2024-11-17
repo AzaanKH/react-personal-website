c49780bd059debb8f8bc77339a57d2ce518efe58
Azaan <azaank@uw.edu>
2024-11-17 00:22:05 -0800

    Updates

---
+++ assets/Resume-q80nPgun.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-DGicexTx.js";const t=()=>e.jsx("section",{id:"resume",className:"section py-5",children:e.jsxs("div",{className:"container text-center",children:[e.jsx("h2",{className:"section-title",children:"Resume"}),e.jsx("a",{href:"/react-personal-website/khalfe_azaan_resume_24.pdf",className:"btn resume-btn",target:"_blank",rel:"noopener noreferrer",children:"View Resume"})]})});export{t as default};
+//# sourceMappingURL=Resume-q80nPgun.js.map
